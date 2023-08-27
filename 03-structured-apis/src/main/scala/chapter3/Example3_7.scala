a7be184a1a08ec4e0da4834ad97eac4abb55fb35
Richard Oliver Legendi <richard_oliver_legendi@epam.com>
2023-08-27 14:00:34 +0200

    Cleaning up code

--- 03-structured-apis/src/main/scala/chapter3/Example3_7.scala
+++ 03-structured-apis/src/main/scala/chapter3/Example3_7.scala
@@ -16,9 +16,9 @@ object Example3_7 {
       println("usage Example3_7 <file path to blogs.json")
       System.exit(1)
     }
-    //get the path to the JSON file
+
     val jsonFile = args(0)
-    //define our schema as before
+
     val schema = StructType(Array(StructField("Id", IntegerType, false),
       StructField("First", StringType, false),
       StructField("Last", StringType, false),
@@ -27,19 +27,15 @@ object Example3_7 {
       StructField("Hits", IntegerType, false),
       StructField("Campaigns", ArrayType(StringType), false)))
 
-    //Create a DataFrame by reading from the JSON file a predefined Schema
     val blogsDF = spark.read.schema(schema).json(jsonFile)
-    //show the DataFrame schema as output
+    
     blogsDF.show(truncate = false)
-    // print the schemas
     println(blogsDF.printSchema)
     println(blogsDF.schema)
-    // Show columns and expressions
+
     blogsDF.select(expr("Hits") * 2).show(2)
     blogsDF.select(col("Hits") * 2).show(2)
     blogsDF.select(expr("Hits * 2")).show(2)
-   // show heavy hitters
-   blogsDF.withColumn("Big Hitters", (expr("Hits > 10000"))).show()
-
+    blogsDF.withColumn("Big Hitters", (expr("Hits > 10000"))).show()
   }
 }
