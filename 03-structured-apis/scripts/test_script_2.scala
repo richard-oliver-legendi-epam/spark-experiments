04753f9b9275286228ebf22e9c2a6494db5852ab
Richard Oliver Legendi <richard_oliver_legendi@epam.com>
2023-08-27 14:32:41 +0200

    Playing with types

---
+++ 03-structured-apis/scripts/test_script_2.scala
@@ -0,0 +1,9 @@
+$SPARK_HOME/bin/spark-shell
+scala> import org.apache.spark.sql.types._
+import org.apache.spark.sql.types._
+scala> val nameTypes = StringType
+nameTypes: org.apache.spark.sql.types.StringType.type = StringType
+scala> val firstName = nameTypes
+firstName: org.apache.spark.sql.types.StringType.type = StringType
+scala> val lastName = nameTypes
+lastName: org.apache.spark.sql.types.StringType.type = StringType
