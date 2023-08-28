dc813bf58eb525ff0da26bfc18510164e43799d9
Richard Oliver Legendi <richard_oliver_legendi@epam.com>
2023-08-28 15:08:52 +0200

    Loading data

---
+++ 04-data-sources/scripts.scala
@@ -0,0 +1,19 @@
+// In Scala
+import org.apache.spark.sql.SparkSession
+val spark = SparkSession
+ .builder
+ .appName("SparkSQLExampleApp")
+ .getOrCreate()
+
+// Path to data set
+val csvFile="/databricks-datasets/learning-spark-v2/flights/departuredelays.csv"
+
+// Read and create a temporary view
+// Infer schema (note that for larger files you may want to specify the schema)
+val df = spark.read.format("csv")
+ .option("inferSchema", "true")
+ .option("header", "true")
+ .load(csvFile)
+
+// Create a temporary view
+df.createOrReplaceTempView("us_delay_flights_tbl")
