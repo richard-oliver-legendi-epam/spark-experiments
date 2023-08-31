18ed57b053f3fc51f9d5153093a8c036d6c9597d
Richard Oliver Legendi <richard_oliver_legendi@epam.com>
2023-08-31 18:26:30 +0200

    Query data

--- 05-external/scripts.scala
+++ 05-external/scripts.scala
@@ -7,3 +7,6 @@ spark.udf.register("cubed", cubed)
 // Create temporary view
 spark.range(1, 9).createOrReplaceTempView("udf_test")
 
+// In Scala/Python
+// Query the cubed UDF
+spark.sql("SELECT id, cubed(id) AS id_cubed FROM udf_test").show()
