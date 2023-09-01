94e262c88350817008735878a43b541941b04196
Richard Oliver Legendi <richard_oliver_legendi@epam.com>
2023-09-01 17:04:53 +0200

    Persistance

--- 07-perf/scala-shell-config.scala
+++ 07-perf/scala-shell-config.scala
@@ -28,3 +28,24 @@ val numDF = spark.range(1000).repartition(16)
 numDF.rdd.getNumPartitions
 
 
+
+// Create a DataFrame with 10M records
+val df = spark.range(1 * 10000000).toDF("id").withColumn("square", $"id" * $"id")
+df.cache() // Cache the data
+df.count() // Materialize the cache
+
+df.count() // Now get it from the cache
+
+
+// --------------------------
+
+import org.apache.spark.storage.StorageLevel
+
+// Create a DataFrame with 10M records
+val df = spark.range(1 * 10000000).toDF("id").withColumn("square", $"id" * $"id")
+df.persist(StorageLevel.DISK_ONLY) // Serialize the data and cache it on disk
+df.count() // Materialize the cache
+
+df.count() // Now get it from the cache
+
+
