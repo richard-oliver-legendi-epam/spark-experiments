722c21dbe28e9718e859ed39832e6db2ff462437
Richard Oliver Legendi <richard_oliver_legendi@epam.com>
2023-09-01 11:39:38 +0200

    Pf mismatch in DS didn't really helped lol

--- 06-datasets/datasets.scala
+++ 06-datasets/datasets.scala
@@ -1,1 +1,5 @@
-case class Bloggers(id:Int, first:String, last:String, url:String, date:String, hits: Int, campaigns:Array[String])
+case class Bloggers(id:BigInt, first:String, last:String, url:String, published:String, hits: BigInt, campaigns:Array[String])
+
+val bloggers = "data/blogs.json"
+
+val bloggersDS = spark.read.format("json").option("path", bloggers).load().as[Bloggers]
