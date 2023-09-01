b46482b22705ee1f9f020b3b1c0748c3f54adb6e
Richard Oliver Legendi <richard_oliver_legendi@epam.com>
2023-09-01 08:20:50 +0200

    Messing with ordering, seems working though with small dataset

--- 05-external/scripts.scala
+++ 05-external/scripts.scala
@@ -1,6 +1,8 @@
 // Create cubed function
 val cubed = (s: Long) => { s * s * s }
 
+val cubedWithNull : Long => String = (s: Long) => { if (s == 3) null else "Pompli_" + s }
+
 // Register UDF
 spark.udf.register("cubed", cubed)
 
@@ -10,3 +12,17 @@ spark.range(1, 9).createOrReplaceTempView("udf_test")
 // In Scala/Python
 // Query the cubed UDF
 spark.sql("SELECT id, cubed(id) AS id_cubed FROM udf_test").show()
+
+
+List(1l,2l,3l,4l).foreach(x => println(cubedWithNull(x)))
+
+spark.udf.register("cubedWithNull", cubedWithNull)
+
+spark.sql("SELECT id, cubedWithNull(id) AS id_cubed FROM udf_test").show()
+
+spark.sql("SELECT id, cubedWithNull(id) AS id_cubed FROM udf_test").createOrReplaceTempView("udf_test2")
+
+spark.sql("SELECT id, cubedWithNull(id) AS id_cubed FROM udf_test2").show()
+
+spark.sql("SELECT id, cubedWithNull(id) FROM udf_test2 WHERE cubedWithNull(id) IS NOT NULL AND length(cubedWithNull(id)) > 1")
+ 
